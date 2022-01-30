aa24fdf2fb0cad9423b396f078c9b81dfb18fd70
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2022-01-30 13:26:33 +0530

    Update two sum.cpp

--- leetcode easy/two sum.cpp
+++ leetcode easy/two sum.cpp
@@ -19,3 +19,26 @@ public:
         return arr;
     }
 };
+
+*************************************************more efficient******************************************************
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>arr(2);
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if (nums[i]+nums[j]==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                    
+                }
+                
+            }
+           
+        }
+            
+       
+        return arr;
+    }
+};
