69a010ef63b5d7fa60d78dbe5da51708188c6e68
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2022-02-01 23:45:53 +0530

    Update 21. Merge Two Sorted Lists.cpp

--- leetcode easy/21. Merge Two Sorted Lists.cpp
+++ leetcode easy/21. Merge Two Sorted Lists.cpp
@@ -14,3 +14,58 @@ public:
        return list2; 
     }
 };
+
+****************MORE EFFICIENT *******************************
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+         ListNode *head = NULL, *curr = NULL;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                if(head == NULL){
+                    head = list1;
+                    curr = list1;
+                }
+                else {
+                    curr->next = list1;
+                    curr = curr->next;
+                }
+                list1 = list1->next;
+            }
+            else{
+                if(head == NULL){
+                    head = list2;
+                    curr = list2;
+                }
+                else {
+                    curr->next = list2;
+                    curr = curr->next;
+                }
+                list2 = list2->next;
+            }
+        }
+        while(list1!=NULL){
+            if(head == NULL){
+                head = list1;
+                curr = list1;
+            }
+            else {
+                curr->next = list1;
+                curr = curr->next;
+            }
+            list1 = list1->next;
+        }
+        while(list2!=NULL){
+            if(head == NULL){
+                head = list2;
+                curr = list2;
+            }
+            else {
+                curr->next = list2;
+                curr = curr->next;
+            }
+            list2 = list2->next;
+        }
+        return head;
+    }
+};
