066a50269c7fd849176029366238994ad8eab37e
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2022-02-03 21:29:03 +0530

    Create 454 4Sum II.cpp

---
+++ medium/454 4Sum II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int cnt=0;
+        for(int i=0;i<nums1.size();i++)
+        {
+            for(int j=0;j<nums2.size();j++)
+            {
+                for(int k=0;k<nums3.size();k++)
+                {
+                    for(int l=0;l<nums4.size();l++)
+                    {
+                        if(nums1[i]+nums2[j]+nums3[k]+nums4[l]==0)
+                        {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
