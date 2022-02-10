e4b046eb450b3ebdf24678819b73fe7ffacc390b
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2022-02-10 23:18:09 +0530

    Update 1544. Make The String Great.cpp

--- leetcode easy/1544. Make The String Great.cpp
+++ leetcode easy/1544. Make The String Great.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    string makeGood(string s) {
+       string ans="";
+        int i=0;
+        while(i<s.length()){
+            if(ans.length()!=0 && abs(s[i]-ans[ans.length()-1])==32){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+            i++;
+        }
+        return ans; 
+        
+    }
+};
